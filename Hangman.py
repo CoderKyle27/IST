e32149a42300d58493928cbd575f819373e79639
Kyle <kylecraft12@gmail.com>
2018-11-07 11:20:57 +1100

    New Quotes

--- Hangman.py
+++ Hangman.py
@@ -1,15 +1,27 @@
 
+
+#Imports
 import random
 import time
+
+#Variables
 name = input("What is your name? ")
 print("Hello, " + name, "Time to play hangman!")
 time.sleep (1)
 print("Start guessing!")
 time.sleep (0.5)
+
+#Words
 WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
+
+#Chooses the random words
 word = random.choice(WORDS)
 guesses = ''
+
+#Number of turns
 turns = 10
+
+#Else statement
 while turns > 0:
     failed = 0
     for char in word:
@@ -22,6 +34,8 @@ while turns > 0:
         print("You won")
         break
         print
+
+# Turns and how they work
         guess = input("guess a character:")
         guesses += guess
         if guess not in word:
